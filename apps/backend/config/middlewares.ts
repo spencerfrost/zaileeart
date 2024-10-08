934422b6f26abe26b06c0fdd1461b6228ae1c81b
Spencer Frost <34923439+spencerfrost@users.noreply.github.com>
2024-10-07 20:02:48 -0400

    Enable CORS middleware with specific origin and add admin path to Strapi configuration

--- apps/backend/config/middlewares.ts
+++ apps/backend/config/middlewares.ts
@@ -2,11 +2,18 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['https://zaileeart.mrspinn.ca']
+    }
+  }
 ];
--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -11,6 +11,7 @@ export default defineNuxtConfig({
   strapi: {
     url: process.env.NUXT_PUBLIC_STRAPI_URL ?? 'http://localhost:1338',
     version: 'v4',
+    admin: '/admin',
     cookie: {},
     cookieName: 'strapi_jwt'
   },
