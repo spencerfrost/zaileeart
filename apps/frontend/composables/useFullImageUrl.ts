39aabf7ec193f6dc1d2fb39b1f0662697e8a94fc
Spencer Frost <34923439+spencerfrost@users.noreply.github.com>
2024-10-08 19:57:25 -0400

    Simplify path construction in useFullImageUrl by removing production check

--- apps/frontend/composables/useFullImageUrl.ts
+++ apps/frontend/composables/useFullImageUrl.ts
@@ -11,10 +11,6 @@ export function useFullImageUrl() {
     }
     
     // if process.env.NODE_ENV is 'production', add the 'public' prefix
-    if (config.public.environment === 'production') {
-      return `${config.public.strapiURL}/public${path}`
-    } else {
-      return `${config.public.strapiURL}${path}`
-    }
+    return `${config.public.strapiURL}${path}`
   }
 }
